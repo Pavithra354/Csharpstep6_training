8f4ca8cd62749f91a28a84368fee364cce162890
Pavithra354 <naralapavithra@gmail.com>
2025-07-26 15:02:00 +0530

    commit

---
+++ Step6/Deligates/Anonymous.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    delegate void RestaurantTask(string taskDetail);
+
+    class Program
+    {
+        static void Main()
+        {
+            // Anonymous method for taking orders
+            RestaurantTask takeOrder = delegate (string orderName)
+            {
+                Console.WriteLine("Waiter: Taking order for " + orderName);
+            };
+
+            // Anonymous method for preparing food
+            RestaurantTask prepareFood = delegate (string orderName)
+            {
+                Console.WriteLine("Kitchen: Preparing " + orderName);
+            };
+
+            // Anonymous method for serving food
+            RestaurantTask serveFood = delegate (string orderName)
+            {
+                Console.WriteLine("Waiter: Serving " + orderName);
+            };
+
+            string order = "Pista";
+            takeOrder(order);
+            prepareFood(order);
+            serveFood(order);
+        }
+    }
+}
